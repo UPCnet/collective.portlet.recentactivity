bd19b40f1bb5f87bf2dd18ede14e54333d83db31
Timo Stollewerk <contact@timostollenwerk.net>
2009-11-19 14:31:32 +0000

    show viewlet only to logged in users.

--- collective/portlet/recentactivity/viewlet.py
+++ collective/portlet/recentactivity/viewlet.py
@@ -7,12 +7,24 @@ from collective.portlet.recentactivity.interfaces import IRecentActivityUtility
 from Acquisition import aq_inner
 from plone.memoize.instance import memoize
 
+from zope.component import getMultiAdapter
+
 import time
 
 class RecentActivityViewlet(ViewletBase):
     index = ViewPageTemplateFile('viewlet.pt')
 
+    @property
+    def available(self):
+        """Show the portlet only to logged in users.
+        """
+        context = aq_inner(self.context)        
+        portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        return not portal_state.anonymous()
+    
     def recent_activities(self):
+        """Recent activities, most recent activities come first.
+        """
         context = aq_inner(self.context)        
         for brain in self._data():
             activity = brain[1]
